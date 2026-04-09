4083d14718e6b0104e5550a0ba0a9286485e6561
Thales <thalessm@gmail.com>
2026-04-08 21:33:40 -0300

    Add files via upload

--- avatar-map-data.js
+++ avatar-map-data.js
@@ -1,24 +1,33 @@
 window.VIDA_CORRIDA_AVATAR_DATA = {
-  byId: {},
-  byEmail: {},
-  byName: {
-    "Adilson Marandini": "adilson-marandini.jpg",
-    "Ana Milano": "ana_milano.jpg",
-    "Camila Magroski": "camila_magroski.jpg",
-    "Carla Fenzke": "carla-fenzke.jpg",
-    "Catia Fenzke": "catia-fenzke.jpg",
-    "Catia Silene Fenzke Mirapalheta": "catia-fenzke.jpg",
-    "Cintia Campos": "cintia_campos.jpg",
-    "Cintia Correa Campos": "cintia_campos.jpg",
-    "Felype Nascimento": "felype-nascimento.jpg",
-    "Janaina Garcia Mascarenhas": "janaina_mascarenhas.jpg",
-    "Joana Ribeiro": "joana-ribeiro.jpg",
-    "Julio Araujo": "julio-araujo.jpg",
-    "Lorraine Rosa": "lorraine_rosa.jpg",
-    "Lorraine Silva Da Rosa": "lorraine_rosa.jpg",
-    "Luisa Pinto da Silva": "luisa-pinto-da-silva.jpg",
-    "Nathalia Vergara": "nathalia-vergara.jpg",
-    "Thales Mirapalheta": "thales-mirapalheta.jpg",
-    "Thales Soares Mirapalheta": "thales-mirapalheta.jpg"
-  }
+    "byId":  {
+
+             },
+    "byEmail":  {
+
+                },
+    "byName":  {
+                   "Adilson Marandini":  "adilson-marandini.jpg",
+                   "Ana Milano":  "ana_milano.jpg",
+                   "Andre Alvarenga":  "andre_alvarenga.jpg",
+                   "Angela Potter":  "angela_potter.jpg",
+                   "Camila Magroski":  "camila_magroski.jpg",
+                   "Carla Fenzke":  "carla-fenzke.jpg",
+                   "Catia Fenzke":  "catia-fenzke.jpg",
+                   "Catia Silene Fenzke Mirapalheta":  "catia-fenzke.jpg",
+                   "Cintia Campos":  "cintia_campos.jpg",
+                   "Cintia Correa Campos":  "cintia_campos.jpg",
+                   "Claudio Corrêa Simões":  "claudio_simoes.jpg",
+                   "Eduarda Constantino":  "eduarda_constantino.jpg",
+                   "Felype Nascimento":  "felype-nascimento.jpg",
+                   "Janaina Garcia Mascarenhas":  "janaina_mascarenhas.jpg",
+                   "Joana Ribeiro":  "joana-ribeiro.jpg",
+                   "Julio Araujo":  "julio-araujo.jpg",
+                   "Lorraine Rosa":  "lorraine_rosa.jpg",
+                   "Lorraine Silva Da Rosa":  "lorraine_rosa.jpg",
+                   "Luisa Pinto da Silva":  "luisa-pinto-da-silva.jpg",
+                   "Miguel Angelo Castro Jr.":  "miguel_castro.jpg",
+                   "Nathalia Vergara":  "nathalia-vergara.jpg",
+                   "Thales Mirapalheta":  "thales-mirapalheta.jpg",
+                   "Thales Soares Mirapalheta":  "thales-mirapalheta.jpg"
+               }
 };
